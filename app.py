d13afb0de67b31c670088d80c1da85daaadfdce8
XxAtomm <92411982+xxatomm@users.noreply.github.com>
2024-11-09 22:01:41 -0500

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def log_visitor():
-    # Obtener información del visitante
     ip_address = request.remote_addr
     user_agent = request.headers.get('User-Agent')
     referrer = request.referrer if request.referrer else "Sin referer"
@@ -14,12 +13,8 @@ def log_visitor():
     method = request.method
     headers = dict(request.headers)
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    # Información del servidor
     server_ip = socket.gethostbyname(socket.gethostname())
     server_host = socket.gethostname()
-
-    # Formatear la información para el archivo de logs
     log_entry = f"""
 --- NUEVO REGISTRO ---
 Fecha y Hora: {timestamp}
@@ -35,14 +30,11 @@ Nombre del Host del Servidor: {server_host}
 -----------------------
 """
 
-    # Guardar la información en un archivo de logs
     with open('logs.txt', 'a') as log_file:
         log_file.write(log_entry)
 
-    # Redirigir al usuario a una página externa
     return redirect('https://www.microsoft.com/es-pe')
 
 if __name__ == '__main__':
-    # Ejecutar la aplicación en modo HTTP para evitar advertencias de seguridad en entornos locales
     app.run(host='0.0.0.0', port=5000)
 
