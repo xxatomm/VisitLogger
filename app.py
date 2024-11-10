0d02d630b056fd119077d677bff2b8dec9be1ff9
XxAtomm <92411982+xxatomm@users.noreply.github.com>
2024-11-09 21:38:47 -0500

    app.py

--- app.py
+++ app.py
@@ -4,29 +4,15 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def log_visitor():
+def log_ip():
     ip_address = request.remote_addr
     user_agent = request.headers.get('User-Agent')
-    referrer = request.referrer if request.referrer else "No referer"
-    language = request.headers.get('Accept-Language')
-    method = request.method
-    headers = dict(request.headers)
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     with open('logs.txt', 'a') as log_file:
-        log_file.write(f"""
---- NUEVO REGISTRO ---
-IP: {ip_address}
-User Agent: {user_agent}
-Referer: {referrer}
-Idioma: {language}
-Método de Solicitud: {method}
-Hora: {timestamp}
-Encabezados: {headers}
------------------------
-""")
+        log_file.write(f"IP: {ip_address} - User Agent: {user_agent} - Time: {timestamp}\n")
 
     return redirect('https://www.microsoft.com/es-pe')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
